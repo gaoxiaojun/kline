0dd78137964b3c4e62c4900c10ec9955a0f35185
forex24 <admin@forex24.today>
2021-07-28 01:06:58 +0800

    prepare for rewrite

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,15 +1,18 @@
 use crate::bar::Bar;
 use crate::candle::Candle;
 use crate::fractal::Fx;
+use crate::ringbuffer::RingBuffer;
 
 #[derive(Debug)]
 pub struct Analyzer {
     bars: Vec<Bar>,
-    candles:Vec<Candle>,
-    next_index:u64,
-    fx_list:Vec<Fx>,
-    bi_list:Vec<Fx>,
-    xd_list:Vec<Fx>
+    candles: Vec<Candle>,
+    next_index: u64,
+    fx_list: Vec<Fx>,
+    bi_list: Vec<Fx>,
+    xd_list: Vec<Fx>,
+    //
+    window: RingBuffer<Candle>,
 }
 
 impl Analyzer {
@@ -17,18 +20,76 @@ impl Analyzer {
         Self {
             bars: Vec::new(),
             candles: Vec::new(),
-            fx_list:Vec::new(),
-            next_index:0,
-            bi_list:Vec::new(),
-            xd_list:Vec::new(),
+            fx_list: Vec::new(),
+            next_index: 0,
+            bi_list: Vec::new(),
+            xd_list: Vec::new(),
+            window: RingBuffer::new(3),
         }
     }
 
-    fn update_candle(&mut self){}
+    pub fn get_candles(&self) -> &Vec<Candle> {
+        &self.candles
+    }
 
-    fn update_fx(&mut self){}
+    fn update_candle(&mut self) {}
 
-    fn update_bi_list(&mut self){}
+    fn update_fx(&mut self) {}
 
-    fn update_xd_list(&mut self){}
+    fn update_bi_list(&mut self) {}
+
+    fn update_xd_list(&mut self) {}
+
+    fn add_candle(&mut self, bar: &Bar) {
+        let c = Candle::from_bar(self.next_index, bar);
+        self.next_index += 1;
+        self.candles.push(c.clone());
+        self.window.push(c);
+    }
+    // 检查是否为顶底分型
+    fn check_fractal(&self) -> Option<Fx> {
+        let k1 = self.window.get(-3).unwrap();
+        let k2 = self.window.get(-2).unwrap();
+        let k3 = self.window.get(-1).unwrap();
+
+        Fx::check_fractal(k1, k2, k3)
+    }
+
+    // 处理与当前bar的包含关系
+    fn process_contain_relationship(&mut self, bar: &Bar) -> bool {
+        // 队列中有至少两个经过包含处理的Candle
+        debug_assert!(self.window.len() >= 2);
+        let direction = {
+            let k1 = self.window.get(-2).unwrap();
+            let k2 = self.window.get(-1).unwrap();
+            Candle::check_direction(k1, k2)
+        };
+
+        let current = self.window.get_mut(-1).unwrap();
+
+        Candle::merge(direction, current, bar)
+    }
+    pub fn on_new_bar(&mut self, bar: &Bar) -> Option<Fx> {
+        self.bars.push(bar.clone());
+        let wlen = self.window.len();
+        match wlen {
+            0 | 1 => self.add_candle(&bar),
+            2 => {
+                let merged = self.process_contain_relationship(bar);
+                if !merged {
+                    self.add_candle(&bar);
+                }
+            }
+
+            _ => {
+                let merged = self.process_contain_relationship(bar);
+                if !merged {
+                    let result = self.check_fractal();
+                    self.add_candle(&bar);
+                    return result;
+                }
+            }
+        }
+        None
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use kline::util::load_bar_from_csv;
 #[macro_use] extern crate manifest_dir_macros;
 const EU_DATA: &str = path!("data","EU_2021.csv");
 use kline::time::timestamp_to_utc;
-
+use kline::analyzer::Analyzer;
 fn main() {
     // load test data
     let bars = load_bar_from_csv(EU_DATA).unwrap();
@@ -11,4 +11,9 @@ fn main() {
     let end_time  = timestamp_to_utc(bars.last().unwrap().time);
     println!("Load {} bars, start = {}, end = {}", bars_count, start_time, end_time);
     // process test data
+    let mut analyzer = Analyzer::new();
+    for bar in &bars {
+        analyzer.on_new_bar(bar);
+    }
+    println!("Candle count: {}", analyzer.get_candles().len());
 }
